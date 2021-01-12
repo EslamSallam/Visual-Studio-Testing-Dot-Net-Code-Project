69f118ab27ca47caab6e75d9fa2587506b38afd8
EslamSallam <eslam.salam1369@gmail.com>
2021-01-12 17:01:31 +0200

    Creating my first test project

---
+++ WiredBrainCoffee.CupOrderAdmin.Core.Tests/Services/OrderCreation/OrderCreationServiceTests.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace WiredBrainCoffee.CupOrderAdmin.Core.Tests.Services.OrderCreation
+{
+    [TestClass]
+    public class OrderCreationServiceTests
+    {
+
+        [TestMethod]
+        public void ShouldStoreCreatedOrderInOrderCreationResult()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
