88c4182851b769147309652ecdd66cb07bf0d943
EslamSallam <eslam.salam1369@gmail.com>
2021-01-18 20:28:38 +0200

    stockExceededTest

--- WiredBrainCoffee.CupOrderAdmin.Core.Tests/Services/OrderCreation/OrderCreationServiceTests.cs
+++ WiredBrainCoffee.CupOrderAdmin.Core.Tests/Services/OrderCreation/OrderCreationServiceTests.cs
@@ -11,25 +11,54 @@ namespace WiredBrainCoffee.CupOrderAdmin.Core.Tests.Services.OrderCreation
     [TestClass]
     public class OrderCreationServiceTests
     {
+        private OrderCreationService _orderCreationService;
+        private int _noOfCupsInStock;
 
-        [TestMethod]
-        public async Task ShouldStoreCreatedOrderInOrderCreationResultAsync()
+        [TestInitialize]
+        public void testInitialize()
         {
+            _noOfCupsInStock = 10;
             var orderRepoMock = new Mock<IOrderRepository>();
             orderRepoMock.Setup(x => x.SaveAsync(It.IsAny<Order>())).ReturnsAsync((Order x) => x);
             var coffeeRepoMock = new Mock<ICoffeeCupRepository>();
+            coffeeRepoMock.Setup(x => x.GetCoffeeCupsInStockCountAsync()).ReturnsAsync(_noOfCupsInStock);
 
-            var orderCreationService = new OrderCreationService(orderRepoMock.Object,coffeeRepoMock.Object);
+            _orderCreationService = new OrderCreationService(orderRepoMock.Object, coffeeRepoMock.Object);
+        }
+        [TestMethod]
+        public async Task ShouldStoreCreatedOrderInOrderCreationResultAsync()
+        {
 
             var customer = new Customer { Id = 99 };
 
-            int noOfOrderCups = 2;
+            int noOfOrderCups = 1;
             var orderCreationResult =
-            await orderCreationService.CreateOrderAsync(customer, noOfOrderCups);
+            await _orderCreationService.CreateOrderAsync(customer, noOfOrderCups);
             Assert.AreEqual(OrderCreationResultCode.Success, orderCreationResult.ResultCode);
             Assert.AreEqual(customer.Id, orderCreationResult.CreatedOrder.CustomerId);
             Assert.IsNotNull(orderCreationResult.CreatedOrder);
 
+        }
+        [TestMethod]
+        public async Task ShouldStoreRemainingCupsInStockInOrderOrderCreationResult()
+        {
+            var customer = new Customer();
+            int noOfOrderCups = 3;
+            var noOfCups = _noOfCupsInStock - noOfOrderCups;
+            var orderCreationResult =
+            await _orderCreationService.CreateOrderAsync(customer, noOfOrderCups);
+            Assert.AreEqual(OrderCreationResultCode.Success, orderCreationResult.ResultCode);
+            Assert.AreEqual(noOfCups, orderCreationResult.RemainingCupsInStock);
+        }
+        [TestMethod]
+        public async Task ShouldReturnStockExceededResultIfNotEnoughCupsInStock()
+        {
+            var customer = new Customer();
+            int noOfOrderCups = _noOfCupsInStock + 1;
+            var orderCreationResult =
+            await _orderCreationService.CreateOrderAsync(customer, noOfOrderCups);
+            Assert.AreEqual(OrderCreationResultCode.StockExceeded, orderCreationResult.ResultCode);
+            Assert.AreEqual(_noOfCupsInStock, orderCreationResult.RemainingCupsInStock);
         }
     }
 }
--- WiredBrainCoffee.CupOrderAdmin.Core/Services/OrderCreation/OrderCreationService.cs
+++ WiredBrainCoffee.CupOrderAdmin.Core/Services/OrderCreation/OrderCreationService.cs
@@ -6,84 +6,97 @@ using WiredBrainCoffee.CupOrderAdmin.Core.Model.Enums;
 
 namespace WiredBrainCoffee.CupOrderAdmin.Core.Services.OrderCreation
 {
-  public class OrderCreationService : IOrderCreationService
-  {
-    private readonly IOrderRepository _orderRepository;
-    private readonly ICoffeeCupRepository _coffeeCupRepository;
-
-    public OrderCreationService(IOrderRepository orderRepository,
-      ICoffeeCupRepository coffeeCupRepository)
+    public class OrderCreationService : IOrderCreationService
     {
-      _orderRepository =
-        orderRepository ?? throw new ArgumentNullException(nameof(orderRepository));
-      _coffeeCupRepository =
-        coffeeCupRepository ?? throw new ArgumentNullException(nameof(coffeeCupRepository));
-    }
+        private readonly IOrderRepository _orderRepository;
+        private readonly ICoffeeCupRepository _coffeeCupRepository;
 
-    public async Task<OrderCreationResult> CreateOrderAsync(Customer customer,
-      int numberOfOrderedCups)
-    {
-      // TODO: Throw ArgumentOutOfRangeException if number of ordered cups is less than 1 /
-
-      OrderCreationResult result;
-
-      // TODO: Return StockExceeded result code if not enough cups in stock
-
-      Order createdOrder = await CreateOrderInternalAsync(customer, numberOfOrderedCups);
-
-      result = new OrderCreationResult
-      {
-        ResultCode = OrderCreationResultCode.Success,
-        CreatedOrder = createdOrder,
-        RemainingCupsInStock = 0 // TODO: Store remaining cups in stock in OrderCreationResult
-      };
-
-      return result;
-    }
-
-    private static double CalculateDiscountPercentage(CustomerMembership membership,
-      int numberOfOrderedCups)
-    {
-      var discountInPercent = 0.0;
-
-      // TODO: Calculate discount in percent
-      //
-      //       Rules:
-      //       3% for more than 4 cups
-      //      
-      //       Premium customers get 5% in addition
-
-      return discountInPercent;
-    }
-
-    private async Task<Order> CreateOrderInternalAsync(Customer customer,
-      int numberOfOrderedCups)
-    {
-      var discount = CalculateDiscountPercentage(customer.Membership, numberOfOrderedCups);
-
-      var savedOrder = await _orderRepository.SaveAsync(
-        new Order
+        public OrderCreationService(IOrderRepository orderRepository,
+          ICoffeeCupRepository coffeeCupRepository)
         {
-          CustomerId = customer.Id,
-          DiscountInPercent = discount,
-          Status = OrderStatus.Open
-        });
+            _orderRepository =
+              orderRepository ?? throw new ArgumentNullException(nameof(orderRepository));
+            _coffeeCupRepository =
+              coffeeCupRepository ?? throw new ArgumentNullException(nameof(coffeeCupRepository));
+        }
 
-      await LinkCoffeeCupsAsync(savedOrder.Id, numberOfOrderedCups);
+        public async Task<OrderCreationResult> CreateOrderAsync(Customer customer,
+          int numberOfOrderedCups)
+        {
+            // TODO: Throw ArgumentOutOfRangeException if number of ordered cups is less than 1 /
 
-      return savedOrder;
+            OrderCreationResult result;
+
+            var noOfCupsInStock = await _coffeeCupRepository.GetCoffeeCupsInStockCountAsync();
+
+            var areEnoughCupsInStock = numberOfOrderedCups <= noOfCupsInStock;
+
+            if (areEnoughCupsInStock)
+            {
+                Order createdOrder = await CreateOrderInternalAsync(customer, numberOfOrderedCups);
+
+                result = new OrderCreationResult
+                {
+                    ResultCode = OrderCreationResultCode.Success,
+                    CreatedOrder = createdOrder,
+                    RemainingCupsInStock = noOfCupsInStock - numberOfOrderedCups
+                };
+            }
+            else
+            {
+                result = new OrderCreationResult
+                {
+                    ResultCode = OrderCreationResultCode.StockExceeded,
+                    RemainingCupsInStock = noOfCupsInStock
+                };
+            }
+
+            return result;
+        }
+
+        private static double CalculateDiscountPercentage(CustomerMembership membership,
+          int numberOfOrderedCups)
+        {
+            var discountInPercent = 0.0;
+
+            // TODO: Calculate discount in percent
+            //
+            //       Rules:
+            //       3% for more than 4 cups
+            //      
+            //       Premium customers get 5% in addition
+
+            return discountInPercent;
+        }
+
+        private async Task<Order> CreateOrderInternalAsync(Customer customer,
+          int numberOfOrderedCups)
+        {
+            var discount = CalculateDiscountPercentage(customer.Membership, numberOfOrderedCups);
+
+            var savedOrder = await _orderRepository.SaveAsync(
+              new Order
+              {
+                  CustomerId = customer.Id,
+                  DiscountInPercent = discount,
+                  Status = OrderStatus.Open
+              });
+
+            await LinkCoffeeCupsAsync(savedOrder.Id, numberOfOrderedCups);
+
+            return savedOrder;
+        }
+
+        private async Task LinkCoffeeCupsAsync(int orderId, int numberOfOrderedCups)
+        {
+            var coffeeCupsForOrder =
+              await _coffeeCupRepository.GetCoffeeCupsInStockAsync(numberOfOrderedCups);
+
+            foreach (var coffeeCup in coffeeCupsForOrder)
+            {
+                coffeeCup.OrderId = orderId;
+                await _coffeeCupRepository.SaveAsync(coffeeCup);
+            }
+        }
     }
-
-    private async Task LinkCoffeeCupsAsync(int orderId, int numberOfOrderedCups)
-    {
-      var coffeeCupsForOrder =
-        await _coffeeCupRepository.GetCoffeeCupsInStockAsync(numberOfOrderedCups);
-
-      foreach (var coffeeCup in coffeeCupsForOrder)
-      {
-        coffeeCup.OrderId = orderId;
-        await _coffeeCupRepository.SaveAsync(coffeeCup);
-      }
-    }
-  }
 }
